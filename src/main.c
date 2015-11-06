fc67a8a7b5edcb965c39c13c8f0f25fd076b304e
mfallet <mfallet@lps.umd.edu>
2015-11-06 09:44:08 -0500

    first

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+// #include "dbg.h"
+
+int main(){
+  int x = 0;
+  int y = 0;
+  int z = 0;
+
+  z = x + y;
+ 
+  printf("Hello World! the variable %i is ", z);
+  
+}
